6287974c5688de83d02980d84e94d6e0443c094a
viva9grup <100474729+viva9grup@users.noreply.github.com>
2022-03-20 15:42:21 +0700

    Update squid3-install.sh

--- squid3-install.sh
+++ squid3-install.sh
@@ -145,3 +145,17 @@ echo
 echo "Thank you for using ServerOk.in Squid Proxy Installer."
 echo "To create a proxy user, run command: squid-add-user"
 echo 
+echo "Add new user"
+squid-add-user
+
+echo "Disable IPV6 if there is"
+IPV6=$(ip addr show dev eth0 | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
+if [[ ! -z ${IPV6} ]];then
+echo "
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1b" >> /etc/sysctl.conf
+if
+
+echo "Done!"
+
